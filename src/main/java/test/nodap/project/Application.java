5e5b6f084ce4a58508c5fa1a2f9255e1c70183dd
강민 <kms0644804@naver.com>
2020-04-04 13:00:16 +0900

    mariadb 서버 ip 포트 연결 완료

--- src/main/java/test/nodap/project/Application.java
+++ src/main/java/test/nodap/project/Application.java
@@ -3,18 +3,7 @@ package test.nodap.project;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.filter.CharacterEncodingFilter;
- 
-import javax.servlet.Filter;
-import java.nio.charset.Charset;
-
-
 
 @SpringBootApplication
 @EnableAutoConfiguration
